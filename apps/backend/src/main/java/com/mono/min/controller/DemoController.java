cf86046fa2f4a4b312b1cf4812e9428115338e37
long <>
2025-02-17 16:27:02 +0700

    init

---
+++ apps/backend/src/main/java/com/mono/min/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.mono.min.controller;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
---
+++ apps/backend/src/test/java/com/mono/min/MinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mono.min;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
